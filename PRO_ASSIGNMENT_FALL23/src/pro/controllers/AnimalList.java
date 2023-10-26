cefbca24932bdeec72e8043bc5005d1d76f77066
kayden <nguyendaithinh3004@gmail.com>
2023-10-26 19:06:48 +0700

    add somthing

--- PRO_ASSIGNMENT_FALL23/src/pro/controllers/AnimalList.java
+++ PRO_ASSIGNMENT_FALL23/src/pro/controllers/AnimalList.java
@@ -5,9 +5,17 @@
  */
 package pro.controllers;
 
+import java.io.File;
 import java.util.ArrayList;
 import pro.models.Animal;
 import pro.models.I_Animal;
+import pro.models.I_Menu;
+import pro.models.Penguin;
+import pro.models.Pigeon;
+import pro.models.Snake;
+import pro.models.Tiger;
+import pro.utils.Utils;
+import static pro.view.ZooManagement.writeToFile;
 
 /**
  *
@@ -16,8 +24,78 @@ import pro.models.I_Animal;
 public class AnimalList extends ArrayList<Animal> implements I_Animal{
 
     @Override
-    public void add() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void add(String url) {
+        I_Menu subMenu = new Menu();
+        subMenu.addItem("\nWhich animal do you wanna update?");
+        subMenu.addItem("1. Snake");
+        subMenu.addItem("2. Tiger");
+        subMenu.addItem("2. Pengiun");
+        subMenu.addItem("3. Pegion");
+        int subChoice;
+        boolean out = true;
+        do {                        
+            subMenu.showMenu();
+            subChoice = subMenu.getChoice();
+            switch (subChoice)
+            {
+                case 1:
+                    String name = Utils.getString("Input your new Snake's name: ");
+                    double weight = Utils.getNumber("Input your new Snake's weight: ", 0, 1000);
+                    String color = Utils.getString("Input your new Snake's color: ");
+                    boolean isCreep = Utils.getBoolean("Your New Snake Can Creep: (Y/N)");
+                    boolean isPoisonous = Utils.getBoolean("Your New Snake has poisonois: (Y/N)");
+                    Snake newSnake = new Snake(isCreep, isPoisonous, name, weight, color);
+                    this.add(newSnake);
+                    break;
+                case 2:
+                    String nameTiger = Utils.getString("Input your new Tiger's name: ");
+                    double weightTiger = Utils.getNumber("Input your new Tiger's weight: ", 0, 1000);
+                    String colorTiger = Utils.getString("Input your new Tiger's color: ");
+                    boolean canRun = Utils.getBoolean("Your New Tiger Can Run: (Y/N)");
+                    boolean canGrowl = Utils.getBoolean("Your New Tiger Can Growl: (Y/N)");
+                    boolean isDangerous = Utils.getBoolean("Your New Tiger Is Dangerous: (Y/N)");
+                    boolean diet = Utils.getBoolean("Your New Tiger Diet: (Y/N)");
+                    boolean eatRawMeat = Utils.getBoolean("Your New Tiger Eat Raw Meat: (Y/N)");
+                    Tiger newTiger = new Tiger(canRun, canGrowl, isDangerous, diet, eatRawMeat, nameTiger, weightTiger, colorTiger);
+                    this.add(newTiger);
+                    break;
+                case 3:
+                    String namePengiun = Utils.getString("Input your new Pengiun's name: ");
+                    double weightPengiun = Utils.getNumber("Input your new Pengiun's weight: ", 0, 1000);
+                    String colorPengiun = Utils.getString("Input your new Pengiun's color: ");  
+                    boolean canWalk = Utils.getBoolean("Your New Pengiun Can Walk: (Y/N)");
+                    boolean canMakeSound = Utils.getBoolean("Your New Pengiun can make soung: (Y/N)");
+                    boolean isCute = Utils.getBoolean("Your New Pengiun is cute: (Y/N)");
+                    Penguin newPenguin = new Penguin(canWalk, canMakeSound, isCute, namePengiun, weightPengiun, colorPengiun);
+                    this.add(newPenguin);
+                    break;
+                case 4:
+                    String namePigeon = Utils.getString("Input your new Pigeon's name: ");
+                    double weightPigeon = Utils.getNumber("Input your new Pigeon's weight: ", 0, 1000);
+                    String colorPigeon = Utils.getString("Input your new Pigeon's color: ");
+                    boolean isHeroic = Utils.getBoolean("Your New Pigeon is heroic: (Y/N)");
+                    boolean canFly = Utils.getBoolean("Your New Pigeon can fly: (Y/N)");
+                    Pigeon newPigeon = new Pigeon(isHeroic, canFly, namePigeon, weightPigeon, colorPigeon);
+                    this.add(newPigeon);
+                    break;
+            }  
+            out = Utils.getBoolean("Do you wanna continue to add animal? (Y/N)");
+        } while (out);
+        File file = new File(url);
+        try {
+            if(file.exists() == false)
+            {
+                file.createNewFile();
+            }
+            writeToFile(url, this);
+        } catch (Exception e) {
+        } 
+        System.out.println("\nYou had added " + this.size() + " animal(s)");
+        for (int i = 0; i < this.size(); i++) {
+            Animal animalResult = this.get(i);
+            System.out.print("\n" + (i + 1) + ". ");
+            animalResult.display();
+        }
     }
 
     @Override
@@ -26,8 +104,18 @@ public class AnimalList extends ArrayList<Animal> implements I_Animal{
     }
 
     @Override
-    public void delete() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean delete(int index) {
+        boolean check = false;
+        for(int i = 0; i < this.size(); i++)
+        {
+            if(i == index - 1)
+            {
+                Animal animal = (Animal) this.get(i);
+                this.remove(animal);
+                check = true;
+            }
+        }
+        return check;
     }
 
     @Override
